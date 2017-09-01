9c8f088765156b60f9fb1eeb1e5747167fedd075
wuzhanwei <zhanwei.wu@dbjtech.com>
2017-09-01 12:05:42 +0800

    chore: update dest

--- dest/setting.js
+++ dest/setting.js
@@ -17,9 +17,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/install",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/install",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\install\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\install\\index.js",
 		"mainDest": "buildtask-install\\index.js",
 		"type": "buildtask"
 	},
@@ -35,9 +35,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/build-webpage",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/build-webpage",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\build-webpage\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\build-webpage\\index.js",
 		"mainDest": "buildtask-build-webpage\\index.js",
 		"type": "buildtask"
 	},
@@ -51,9 +51,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/watch",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/watch",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\watch\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\watch\\index.js",
 		"mainDest": "buildtask-watch\\index.js",
 		"type": "buildtask"
 	},
@@ -61,19 +61,19 @@
 		"name": "buildtask-webpack-babel",
 		"description": "webpack程序的babel编译流程",
 		"devDependencies": {
-			"babel-core": "^6.22.1",
-			"babel-loader": "^6.2.10",
-			"babel-polyfill": "^6.22.0",
-			"babel-preset-env": "^1.5.1",
-			"babel-preset-stage-2": "^6.22.0",
+			"babel-core": "^6.26.0",
+			"babel-loader": "^7.1.2",
+			"babel-polyfill": "^6.26.0",
+			"babel-preset-env": "^1.6.0",
+			"babel-preset-stage-2": "^6.24.1",
 			"lodash": "^4.17.4"
 		},
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/babel",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/babel",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\babel\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\babel\\index.js",
 		"mainDest": "buildtask-webpack-babel\\index.js",
 		"type": "buildtask"
 	},
@@ -81,18 +81,18 @@
 		"name": "buildtask-webpack-build",
 		"description": "webpack程序的基础编译流程",
 		"devDependencies": {
-			"fs-extra": "^3.0.1",
-			"gulp-data": "^1.2.0",
-			"gulp-template": "^3.0.0",
-			"lodash": "^4.6.1",
-			"webpack": "^1.13.1"
+			"fs-extra": "^4.0.1",
+			"gulp-data": "^1.2.1",
+			"gulp-template": "^4.0.0",
+			"lodash": "^4.17.4",
+			"webpack": "^3.5.5"
 		},
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/build",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/build",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\build\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\build\\index.js",
 		"mainDest": "buildtask-webpack-build\\index.js",
 		"type": "buildtask"
 	},
@@ -108,9 +108,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/common-shim",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/common-shim",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\common-shim\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\common-shim\\index.js",
 		"mainDest": "buildtask-webpack-common-shim\\index.js",
 		"type": "buildtask"
 	},
@@ -118,35 +118,19 @@
 		"name": "buildtask-webpack-css",
 		"description": "webpack程序的css加载",
 		"devDependencies": {
-			"css-loader": "^0.23.1",
+			"css-loader": "^0.28.5",
 			"do-nothing-loader": "^1.0.0",
-			"extract-text-webpack-plugin": "^1.0.1",
-			"lodash": "^4.6.1",
-			"style-loader": "^0.13.0"
+			"extract-text-webpack-plugin": "^3.0.0",
+			"lodash": "^4.17.4",
+			"style-loader": "^0.18.2"
 		},
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/css",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/css",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\css\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\css\\index.js",
 		"mainDest": "buildtask-webpack-css\\index.js",
-		"type": "buildtask"
-	},
-	{
-		"name": "buildtask-webpack-json",
-		"description": "webpack程序的json加载",
-		"devDependencies": {
-			"lodash": "^4.6.1",
-			"json-loader": "^0.5.4"
-		},
-		"scripts": {
-			"test": "echo \"Error: no test specified\" && exit 1"
-		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/json",
-		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\json\\index.js",
-		"mainDest": "buildtask-webpack-json\\index.js",
 		"type": "buildtask"
 	},
 	{
@@ -159,9 +143,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/ng-annotate",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/ng-annotate",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\ng-annotate\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\ng-annotate\\index.js",
 		"mainDest": "buildtask-webpack-ng-annotate\\index.js",
 		"type": "buildtask"
 	},
@@ -175,9 +159,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/ng-cache",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/ng-cache",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\ng-cache\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\ng-cache\\index.js",
 		"mainDest": "buildtask-webpack-ng-cache\\index.js",
 		"type": "buildtask"
 	},
@@ -192,9 +176,9 @@
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/resource",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/resource",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\resource\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\resource\\index.js",
 		"mainDest": "buildtask-webpack-resource\\index.js",
 		"type": "buildtask"
 	},
@@ -202,32 +186,29 @@
 		"name": "buildtask-webpack-scss",
 		"description": "webpack程序的scss编译",
 		"devDependencies": {
-			"css-loader": "^0.23.1",
-			"extract-text-webpack-plugin": "^1.0.1",
-			"lodash": "^4.6.1",
+			"css-loader": "^0.28.5",
+			"extract-text-webpack-plugin": "^3.0.0",
+			"lodash": "^4.17.4",
 			"node-sass": "^4.5.3",
-			"resolve-url-loader": "^1.4.3",
-			"sass-loader": "^3.1.2",
-			"style-loader": "^0.13.0"
+			"resolve-url-loader": "^2.1.0",
+			"sass-loader": "^6.0.6",
+			"style-loader": "^0.18.2"
 		},
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/scss",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/scss",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\scss\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\scss\\index.js",
 		"mainDest": "buildtask-webpack-scss\\index.js",
 		"type": "buildtask"
 	},
 	{
 		"name": "buildtask-webpack-split-vendor",
 		"description": "split vendor js from frontend main.js",
-		"dependencies": {
-			"webpack": "^1.13.1"
-		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/split-vendor",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/buildtask/webpack/split-vendor",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\split-vendor\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\buildtask\\webpack\\split-vendor\\index.js",
 		"mainDest": "buildtask-webpack-split-vendor\\index.js",
 		"type": "buildtask"
 	},
@@ -235,16 +216,19 @@
 		"name": "webpack-dev-server",
 		"description": "buildtask for webpack-dev-server",
 		"dependencies": {
-			"http-proxy-middleware": "^0.17.0",
-			"morgan": "^1.7.0",
-			"webpack-dev-server": "^1.14.1"
+			"express": "^4.15.4",
+			"http-proxy-middleware": "^0.17.4",
+			"morgan": "^1.8.2",
+			"webpack-dev-middleware": "^1.12.0",
+			"webpack-dev-server": "^2.7.1",
+			"webpack-hot-middleware": "^2.18.2"
 		},
 		"scripts": {
 			"test": "echo \"Error: no test specified\" && exit 1"
 		},
-		"path": "D:/nodejs/node_modules/brickyard-cli/brickyard_modules/framework/webserver/webpack-dev-server",
+		"path": "C:/Users/squal/AppData/Roaming/npm/node_modules/brickyard-cli/brickyard_modules/framework/webserver/webpack-dev-server",
 		"main": "index.js",
-		"mainSrc": "D:\\nodejs\\node_modules\\brickyard-cli\\brickyard_modules\\framework\\webserver\\webpack-dev-server\\index.js",
+		"mainSrc": "C:\\Users\\squal\\AppData\\Roaming\\npm\\node_modules\\brickyard-cli\\brickyard_modules\\framework\\webserver\\webpack-dev-server\\index.js",
 		"mainDest": "webpack-dev-server\\index.js",
 		"type": "buildtask"
 	}
