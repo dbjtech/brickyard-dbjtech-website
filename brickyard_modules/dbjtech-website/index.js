9445b41b3609f9afe726329ba9acc99222a2f717
ETBox <13318972468@126.com>
2019-12-09 14:37:50 +0800

    feat: add moment.js in devDependencies

--- brickyard_modules/dbjtech-website/index.js
+++ brickyard_modules/dbjtech-website/index.js
@@ -1,4 +1,3 @@
-
 //ie
 require('html5shiv')
 require('imports-loader?this=>window!respond.js/dest/respond.src.js')
@@ -6,15 +5,14 @@ require('imports-loader?this=>window!respond.js/dest/respond.src.js')
 	if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 		var msViewportStyle = document.createElement('style')
 		msViewportStyle.appendChild(
-			document.createTextNode(
-				'@-ms-viewport{width:auto!important}'
-			)
+			document.createTextNode('@-ms-viewport{width:auto!important}'),
 		)
 		document.querySelector('head').appendChild(msViewportStyle)
 	}
 })()
 
 var $ = require('jquery')
+var moment = require('moment')
 require('bootstrap')
 require('bootstrap-autohidingnavbar')
 require('bootstrap/dist/css/bootstrap.css')
@@ -34,11 +32,15 @@ require('./news/main.css')
 
 $(function() {
 	// set title
-	var title = $('#title, h1, h2, h3').first().text()
+	var title = $('#title, h1, h2, h3')
+		.first()
+		.text()
 	$(document).attr('title', title)
 
 	// set copyright
-	var copyright = $('.copyright').html().replace(/^©2016/, '©2016-' + new Date().getFullYear())
+	var copyright = $('.copyright')
+		.html()
+		.replace(/^©2016/, '©2016-' + new Date().getFullYear())
 	$('.copyright').html(copyright)
 
 	// auto hide navbar
